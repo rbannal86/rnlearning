10bc3eafeceb023183434322a18fe6c5cef4282e
Alex Bannow <rbannal@gmail.com>
2020-11-22 14:05:10 -0500

    updated reducer to use standard naming, changed state to reducer on counterscreen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, counter: state.counter + action.payload };
+    case "decrement":
+      return { ...state, counter: state.counter - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
 
   return (
     <View>
       <Button
         title={"Increase"}
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ type: "increment", payload: 1 });
         }}
       />
       <Button
         title={"Decrease"}
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ type: "decrement", payload: 1 });
         }}
       />
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {state.counter}</Text>
     </View>
   );
 };
